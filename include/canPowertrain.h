34ce0be2897b839f31d925bec5224365840b523f
maxda <maxdai510@gmail.com>
2025-04-02 19:37:53 -0500

    CAN for powertrain

--- include/canPowertrain.h
+++ include/canPowertrain.h
@@ -4,6 +4,13 @@
 #include "canmanager.h"
 #include "IOManagement.h"
 
+#define I_OUT_12V_TELEM_ID 0x500
+#define V_OUT_12V_TELEM_ID 0x501
+#define SUPP_I_TELEM_ID 0x502
+#define BATT_I_TELEM_ID 0x503
+#define SUPP_V_TELEM_ID 0x504
+#define POWERTRAIN_BOOLS 0x505
+
 class CANPowertrain : public CANManager {
     public:
         CANPowertrain(CAN_TypeDef* canPort, CAN_PINS pins, int frequency = DEFAULT_CAN_FREQ);
--- src/canPowertrain.cpp
+++ src/canPowertrain.cpp
@@ -2,10 +2,36 @@
 
 CANPowertrain::CANPowertrain(CAN_TypeDef* canPort, CAN_PINS pins, int frequency) : CANManager(canPort, pins, frequency) {};
 
-void CANPowertrain::readHandler(CAN_message_t msg) {
+volatile bool mcu_battery_enable = false;
 
+void CANPowertrain::readHandler(CAN_message_t msg) {
+    uint8_t *data = msg.buf;
+    switch(msg.id){
+        case POWERTRAIN_BOOLS:
+            mcu_battery_enable = (data[0] >> 5) & 1;
+            break;
+        default:
+            break;
+    }
 }
 
 void CANPowertrain::sendPowertrainData() {
     // TODO: send messages with their respective CAN IDs
+    this->sendMessage(I_OUT_12V_TELEM_ID, (void*)&i_out_12v, sizeof(float));
+    this->sendMessage(V_OUT_12V_TELEM_ID, (void*)&v_out_12v, sizeof(float));
+    this->sendMessage(SUPP_I_TELEM_ID, (void*)&supp_i, sizeof(float));
+    this->sendMessage(BATT_I_TELEM_ID, (void*)&batt_i, sizeof(float));
+    this->sendMessage(SUPP_V_TELEM_ID, (void*)&supp_v, sizeof(float));
+
+    u_int8_t boolByte = 0;
+    boolByte |= (batt_neg_cont << 0);
+    boolByte |= (estop_mcu << 1);
+    boolByte |= (batt_pos_cont << 2);
+    boolByte |= (ppc1_supp_invalid << 3);
+    boolByte |= (ppc1_dcdc_invalid << 4);
+    boolByte |= (mppt_cont_mcu << 6);
+    boolByte |= (mc_cont_mcu << 7);
+
+    this->sendMessage(POWERTRAIN_BOOLS, (void*)&boolByte, sizeof(u_int8_t));
+
 }
