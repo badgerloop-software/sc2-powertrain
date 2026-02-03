d138881f81ac697ab4f5807544a94b61a9faf201
germilla <germilla@wisc.edu>
2026-02-02 21:04:43 -0600

    FIxes

--- src/canPowertrain.cpp
+++ src/canPowertrain.cpp
@@ -1,27 +1,33 @@
 #include "canPowertrain.h"
 
+
+bool send_success;
+
+
 CANPowertrain::CANPowertrain(CAN_TypeDef* canPort, CAN_PINS pins, int frequency) : CANManager(canPort, pins, frequency) {};
 
 //volatile bool mcu_battery_enable = false;
 
 void CANPowertrain::readHandler(CAN_message_t msg) {
-    /*uint8_t *data = msg.buf;
-    switch(msg.id){
-        case POWERTRAIN_BOOLS:
-            mcu_battery_enable = (data[0] >> 5) & 1;
+    uint8_t *data = msg.buf;
+    switch(msg.id-0x500){
+        case 5: // digital_data
+            memcpy((void *)&digital_data, msg.buf, sizeof(digital_data));
+            digital_data.mcu_batt_en = !digital_data.mcu_batt_en;
             break;
         default:
             break;
-    }*/
+    }
 }
 
 void CANPowertrain::sendPowertrainData() {
     // TODO: send messages with their respective CAN IDs
-    this->sendMessage(0x500, (void*)&i_12v, sizeof(float));
-    this->sendMessage(0x501, (void*)&v_12v, sizeof(float));
-    this->sendMessage(0x502, (void*)&supp_i, sizeof(float));
-    this->sendMessage(0x503, (void*)&batt_i, sizeof(float));
-    this->sendMessage(0x504, (void*)&supp_v, sizeof(float));
-    this->sendMessage(0x505, (void*)&digital_data, sizeof(digital_data));
-
+    send_success = true;
+    send_success &= this->sendMessage(0x500, (void*)&i_12v, sizeof(float));
+    send_success &= this->sendMessage(0x501, (void*)&v_12v, sizeof(float));
+    send_success &= this->sendMessage(0x502, (void*)&supp_i, sizeof(float));
+    send_success &= this->sendMessage(0x503, (void*)&batt_i, sizeof(float));
+    send_success &= this->sendMessage(0x504, (void*)&supp_v, sizeof(float));
+    send_success &= this->sendMessage(0x505, (void*)&digital_data, sizeof(digital_data));
+    printf("Powertrain Data Send Success: %d\n", send_success);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
 #include <Arduino.h>
+#include <CANPowertrain.h>
+#include <IOManagement.h>
 
 // put function declarations here:
 int myFunction(int, int);
+CANPowertrain canPowertrain(CAN1, DEF);
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(115200);
+  initIO();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  readIO();
+  canPowertrain.sendPowertrainData();
+  canPowertrain.runQueue(50);
 }
 
 // put function definitions here:
