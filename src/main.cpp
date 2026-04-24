783462e420b24e1b615cfd79dab449b4177c41bd
unknown <latzbratz.2517@gmail.com>
2026-04-24 13:13:30 -0500

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <Arduino.h>
+#include "IOManagement.h"
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(100);
+  
+  Serial.println("Starting setup...");
+  
+  pinMode(MCU_BATT_EN, OUTPUT);
+  digitalWrite(MCU_BATT_EN, HIGH);
+  
+  Serial.println("Setup complete. MCU_BATT_EN is now HIGH and should stay HIGH");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Keep the pin HIGH at all times
+  digitalWrite(MCU_BATT_EN, HIGH);
+  delay(100);
 }
 
 // put function definitions here:
