ef31dc7a4d658652a3d65359b6fe4129d6572124
Will Wentink <wwentink@wisc.edu>
2026-07-18 17:03:35 -0500

    actually send data

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,15 @@
 #include <Arduino.h>
+#include "canPowertrain.h"
 #include "IOManagement.h"
 
-// put function declarations here:
-int myFunction(int, int);
+CANPowertrain canPowertrain(CAN1, DEF);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  delay(100);
-  
-  Serial.println("Starting setup...");
-  
-  pinMode(MCU_BATT_EN, OUTPUT);
-  digitalWrite(MCU_BATT_EN, HIGH);
-  
-  Serial.println("Setup complete. MCU_BATT_EN is now HIGH and should stay HIGH");
+  initIO();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Keep the pin HIGH at all times
-  digitalWrite(MCU_BATT_EN, HIGH);
-  delay(100);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  canPowertrain.sendPowertrainData();
+  canPowertrain.runQueue(50);
 }
